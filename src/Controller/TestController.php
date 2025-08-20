273ad90cc93539270a275770d5c77bec003a4603
Marion Leblanc <marion.leblanc77@gmail.com>
2025-08-20 12:11:47 +0200

    Debugging

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,14 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/', name: 'test')]
 class TestController extends AbstractController
 {
-    #[Route('/', name: '1', methods: "GET")]
-    public function test(
-        
-    ): JsonResponse {
-
+    #[Route('/', name: 'test', methods: ['GET'])]
+    public function test(): JsonResponse {
         return $this->json(['success' => 'Test worked'], 200);
     }
 }
