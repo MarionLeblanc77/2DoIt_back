d8726a933e5830fba5e59a8b60e66bf917922771
Marion Leblanc <marion.leblanc77@gmail.com>
2025-08-20 21:56:25 +0200

    Dirty index.php solution

--- public/index.php
+++ public/index.php
@@ -6,8 +6,9 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-header('Access-Control-Allow-Origin: https://nx0dwjbucbs.preview.infomaniak.website');
-
+if ($_SERVER['APP_ENV'] === 'prod') {
+    header('Access-Control-Allow-Origin: https://nx0dwjbucbs.preview.infomaniak.website');
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
