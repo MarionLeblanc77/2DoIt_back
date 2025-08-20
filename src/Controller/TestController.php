589ab249deeb749258367ab2751560632eba3ea5
Marion Leblanc <marion.leblanc77@gmail.com>
2025-08-20 19:27:10 +0200

    Change regex nelmio cors, clean .env and add test route

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TestController extends AbstractController
@@ -11,5 +13,11 @@ class TestController extends AbstractController
     #[Route('/', name: 'test', methods: ['GET'])]
     public function test(): JsonResponse {
         return $this->json(['success' => 'Test worked'], 200);
+    }
+    #[Route('/users', name: 'browse', methods: "GET")]
+    public function browse(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->json($users, Response::HTTP_OK, [], ["groups" => ["user_read"]]);
     }
 }
