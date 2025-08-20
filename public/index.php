b41c2375c873dae0e6ce8021cffdf52c024a81e7
Marion Leblanc <marion.leblanc77@gmail.com>
2025-08-20 21:50:02 +0200

    Dirty index.php solution

--- public/index.php
+++ public/index.php
@@ -2,7 +2,13 @@
 
 use App\Kernel;
 
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+if (env('APP_ENV') === 'prod') {
+    header('Access-Control-Allow-Origin: https://nx0dwjbucbs.preview.infomaniak.website');
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
