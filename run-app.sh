49a0b3204dc58c998c7b9806a4a5f25fa81b845e
Marion Leblanc <marion.leblanc77@gmail.com>
2025-08-20 14:44:39 +0200

    Reset pre-Railway

--- run-app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Make sure this file has executable permissions, run `chmod +x run-app.sh`
-# Run migrations, process the Nginx configuration template and start Nginx
-php bin/console doctrine:migrations:migrate --no-interaction && (php-fpm -y /assets/php-fpm.conf)
