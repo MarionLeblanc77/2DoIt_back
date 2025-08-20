103cb82d97a02bec31a8ce7b7a466399759e11b7
Marion Leblanc <marion.leblanc77@gmail.com>
2025-08-20 12:38:08 +0200

    Debugging

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x run-app.sh`
 # Run migrations, process the Nginx configuration template and start Nginx
-php bin/console doctrine:migrations:migrate --no-interaction && node /assets/scripts/prestart.mjs && (php-fpm -y /assets/php-fpm.conf)
+php bin/console doctrine:migrations:migrate --no-interaction && (php-fpm -y /assets/php-fpm.conf)
