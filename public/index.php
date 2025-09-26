c6b8339c9d46a9ac23fcf610ba5a4aafe2c8d8f9
Marion Leblanc <marion.leblanc77@gmail.com>
2025-09-26 18:11:25 +0200

    Back to manual with also credential true

--- public/index.php
+++ public/index.php
@@ -4,13 +4,14 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-// if (isset($_SERVER['APP_ENV'])) {
-//     if ($_SERVER['APP_ENV'] === 'prod') {
-//         header('Access-Control-Allow-Origin: https://2doit.mlnc-dev.fr');
-//     } else {
-//         header('Access-Control-Allow-Origin: http://localhost:5173');
-//     }
-// }
+if (isset($_SERVER['APP_ENV'])) {
+    if ($_SERVER['APP_ENV'] === 'prod') {
+        header('Access-Control-Allow-Origin: https://2doit.mlnc-dev.fr');
+        header('Access-Control-Allow-Credentials: true');
+    } else {
+        header('Access-Control-Allow-Origin: http://localhost:5173');
+    }
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
